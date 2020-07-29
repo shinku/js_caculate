3ea3b79e500e1f626211522e94be221e9e898572
shinku <jsgxx00@yeah.net>
2020-07-30 01:22:07 +0800

    Create bob.js
    冒泡算法

---
+++ bob.js
@@ -0,0 +1,15 @@
+//冒泡算法
+//两重循环
+
+function BobSort(arr){
+  for(let i =0;i<arr.length-1;i++){
+    for(let j =i+1;j<arr.length;j++){
+      if(arr[i]>arr[j]){
+        let temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+      }
+    }
+  }
+  return arr;
+}
